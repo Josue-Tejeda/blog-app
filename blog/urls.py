1e8ffade9c62080bec9dacb6303caffea40e82df
JoxueTe <josuetejeda02@gmail.com>
2024-09-03 15:01:29 -0500

    feat: add smtp configuration and view/template to send emails

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,11 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post_slug>',
         views.post_detail, 
         name='post_detail'
     ),
+    path('<int:id>/share/', views.post_share, name='post_share')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.views.generic import ListView
 
 from blog.data import PUBLISHED
+from .forms import EmailPostForm
 from .models import Post
 
 def post_list(request):
@@ -11,10 +14,26 @@ def post_list(request):
     post_list = Post.objects.get_published()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try: 
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     
     context = {'posts': posts}
     return render(request, template, context)
+
+
+class PostListView(ListView):
+    """
+    Args:
+        ListView (Post): View to show list of published posts
+    """
+    queryset = Post.objects.get_published()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'post/list.html'
 
 
 def post_detail(request, year, month, day, post_slug):
@@ -32,4 +51,38 @@ def post_detail(request, year, month, day, post_slug):
     
     context = {'post': post}
     
+    return render(request, template, context)
+
+
+def post_share(request, post_id):
+    template = 'post/share.html'
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=PUBLISHED
+    )
+    sent = False
+    context = {
+        'post': post,
+        'form': form,
+    }
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                "{cd['name']} recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+    
+    context['sent'] = sent
     return render(request, template, context)
